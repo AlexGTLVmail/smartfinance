db0d4b1d1fff8f9ddfb2f9dbe3f692feb9938b4c
AlexGTLVmail <rihter@bk.ru>
2024-09-19 00:56:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("JavaScript файл подключен успешно!");
 // ПОДКЛЮЧЕНИЕ КНОПОК
 //НА ССЫЛКИ
 // document.getElementById('myButton').addEventListener('click', function() {
-//   window.location.href = 'https://example.com';
+// window.location.href = 'https://b24-2e5694.bitrix24.site/crm_form_j3w26/';
 // });
 
 //НА ЗВОНКИ
@@ -10,82 +10,9 @@ document.getElementById('callButton').addEventListener('click', function() {
   window.location.href = 'tel:+79286642202';
 });
 
-// Кнопка для скачивания первого файла
-// document.getElementById('downloadButton1').addEventListener('click', function() {
-//   const link = document.createElement('a');
-//   link.href = './files/file1.pdf';  
-//   link.download = 'file1.pdf';
-//   link.click();
-// });
-
+//НА ЗВОНКИ
 
 // КНОПКИ МОДАЛЬНЫХ ОКОН
-
-// // Получаем модальные окна
-// const modal1 = document.getElementById('profit_growth');
-// const modal2 = document.getElementById('balance_turnover');
-// const modal3 = document.getElementById('modal3');
-
-// // Получаем кнопки по их уникальным классам
-// const buttonOne = document.querySelector('.grid-btn_a');
-// const buttonTwo = document.querySelector('.grid-btn_b');
-// const buttonThree = document.querySelector('.button-three');
-
-// // Функция для открытия модального окна
-// function openModal(modal) {
-//     modal.style.display = 'flex';
-// }
-
-// // Функция для закрытия модального окна
-// function closeModal(modal) {
-//     modal.style.display = 'none';
-// }
-
-// // Добавляем обработчики на каждую кнопку для открытия соответствующего модального окна
-// buttonOne.addEventListener('click', () => {
-//     openModal(modal1);
-// });
-
-// buttonTwo.addEventListener('click', () => {
-//     openModal(modal2);
-// });
-
-// buttonThree.addEventListener('click', () => {
-//     openModal(modal3);
-// });
-
-// // Добавляем обработчики на закрытие окна по клику на крестик
-// modal1.querySelector('.close').addEventListener('click', () => {
-//     closeModal(modal1);
-// });
-
-// modal2.querySelector('.close').addEventListener('click', () => {
-//     closeModal(modal2);
-// });
-
-// modal3.querySelector('.close').addEventListener('click', () => {
-//     closeModal(modal3);
-// });
-
-// // Закрытие окна при клике вне модального контента
-// window.addEventListener('click', (event) => {
-//     if (event.target === modal1) {
-//         closeModal(modal1);
-//     }
-//     if (event.target === modal2) {
-//         closeModal(modal2);
-//     }
-//     if (event.target === modal3) {
-//         closeModal(modal3);
-//     }
-// });
-
-// КНОПКИ МОДАЛЬНЫХ ОКОН
-
-
-
-
-
 
 // Получаем модальные окна и кнопки
 
@@ -141,6 +68,35 @@ window.addEventListener('click', (event) => {
         closeModal();
     }
 });
-
-
 // КНОПКИ МОДАЛЬНЫХ ОКОН
+
+
+
+// ПЫТАЮСЬ ИЗМЕНИТЬ ФОРМУ ЗАЯВКИ НА BITRIX
+
+
+ // Получаем модальное окно
+ var modal = document.getElementById("crmModal");
+
+ // Получаем кнопку, которая открывает модальное окно
+ var btn = document.getElementById("open-crm-form");
+
+ // Получаем элемент <span>, который закрывает модальное окно
+ var span = document.getElementsByClassName("close")[0];
+
+ // Когда пользователь нажимает на кнопку, открываем модальное окно
+ btn.onclick = function() {
+     modal.style.display = "block";
+ }
+
+ // Когда пользователь нажимает на <span> (x), закрываем модальное окно
+ span.onclick = function() {
+     modal.style.display = "none";
+ }
+
+ // Когда пользователь нажимает в любом месте за пределами модального окна, закрываем его
+ window.onclick = function(event) {
+     if (event.target == modal) {
+         modal.style.display = "none";
+     }
+ }
